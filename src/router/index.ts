3cd6ce462d91254c39af4ce7f0a0d6992f23d843
ihzamaulana <ihzamaulana@refactory.id>
2025-04-15 13:48:19 +0700

    update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,9 @@ const routes = [
   { path: "/", component: HomePage, name: "home" },
   { path: "/services", component: ServicePage, name: "service" },
   { path: "/about", component: AboutPage, name: "about" },
-  // { path: "/#contact", component: HomePage, name: "contact" },
-  // { path: "/:pathMatch(.*)*", name: "notfound", component:  },
 ];
 
 const router = createRouter({
-  linkActiveClass: 'transition ease-in-out delay-50 hover:bg-transparent rounded lg:bg-white py-1.5 px-9 rounded-3xl',
   history: createWebHistory(),
   routes,
   scrollBehavior(to) {
